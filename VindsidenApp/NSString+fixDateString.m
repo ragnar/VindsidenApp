911b412dccf93dedf184c9b75dca85d72bc73f35
Ragnar Henriksen <ragnar@shortcut.no>
2013-08-17 11:57:18 +0200

    Adjust timestamp string to support several formats

--- VindsidenApp/NSString+fixDateString.m
+++ VindsidenApp/NSString+fixDateString.m
@@ -13,10 +13,14 @@
 
 - (NSString *)fixDateString
 {
-    //NSString *dateString = [self stringByReplacingCharactersInRange:NSMakeRange(19, 6) withString:([[NSTimeZone localTimeZone] isDaylightSavingTime] ? @"+0200" : @"+0100")];
-    //NSString *dateString = [self stringByReplacingCharactersInRange:NSMakeRange(19, 6) withString:([[NSTimeZone localTimeZone] isDaylightSavingTime] ? @"+0000" : @"+0000")];
-    //NSString *dateString = [self stringByAppendingString:@"+0000"];
-    return self;
+    NSString *ds = nil;
+
+    if ( [self length] == 25 ) {
+        ds = [self stringByReplacingCharactersInRange:NSMakeRange(19, 6) withString:@"+0000"];
+    } else if ( [self rangeOfString:@"Z"].location != NSNotFound ) {
+        ds = [self stringByReplacingCharactersInRange:NSMakeRange(19, 1) withString:@"+0000"];
+    }
+    return [ds stringByReplacingOccurrencesOfString:@"T" withString:@" "];
 }
 
 
