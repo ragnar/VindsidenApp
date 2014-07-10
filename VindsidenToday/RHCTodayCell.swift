932c4649594b02f222207898fffd157b1497819d
Ragnar Henriksen <ragnar@shortcut.no>
2014-07-10 11:45:13 +0200

    Adds Today cell

---
+++ VindsidenToday/RHCTodayCell.swift
@@ -0,0 +1,16 @@
+//
+//  RHCTodayCell.swift
+//  VindsidenApp
+//
+//  Created by Ragnar Henriksen on 10.07.14.
+//  Copyright (c) 2014 RHC. All rights reserved.
+//
+
+import UIKit
+
+class RHCTodayCell: UITableViewCell {
+
+    @IBOutlet var arrowImageView: UIImageView
+    @IBOutlet var speedLabel: UILabel
+    @IBOutlet var nameLabel: UILabel
+}
