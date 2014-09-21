1c9a65d1a0de1228326516634f4a22f407fee2c8
Ragnar Henriksen <ragnar@shortcut.no>
2014-09-21 16:59:34 +0200

    Adds landscape graph view controller

---
+++ VindsidenApp/RHCLandscapeGraphViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RHCLandscapeGraphViewController.swift
+//  VindsidenApp
+//
+//  Created by Ragnar Henriksen on 21/09/14.
+//  Copyright (c) 2014 RHC. All rights reserved.
+//
+
+import UIKit
+import VindsidenKit
+
+
+@objc(RHCLandscapeGraphViewController)
+class RHCLandscapeGraphViewController: UIViewController
+{
+    @IBOutlet weak var graphView: RHEGraphView?
+    @IBOutlet weak var stationName: UILabel?
+
+    var station: CDStation?
+    var plots: NSArray?
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        graphView?.plots = plots
+        stationName?.text = station?.stationName
+    }
+
+}
