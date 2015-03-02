aab764952fab1725767f36cce42899d3ec0dfabb
Ragnar Henriksen <ragnarhenriksen@mac.com>
2015-03-02 08:19:19 +0100

    [WatchKit] Updates glance controller

--- VindsidenApp WatchKit Extension/GlanceController.swift
+++ VindsidenApp WatchKit Extension/GlanceController.swift
@@ -8,24 +8,76 @@
 
 import WatchKit
 import Foundation
+import VindsidenKit
 
 
 class GlanceController: WKInterfaceController {
 
+    @IBOutlet weak var glanceHeadingLabel: WKInterfaceLabel!
+    @IBOutlet weak var glanceWindCurrentLabel: WKInterfaceLabel!
+    @IBOutlet weak var glanceWindDirectionImage: WKInterfaceImage!
+
+
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
     }
+
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+
+        if let station = populateStation() {
+            self.updateUI(station)
+
+            let userInfo = ["station": station.stationId]
+            self.updateUserActivity("AppConfiguration.UserActivity.watch", userInfo: userInfo, webpageURL: nil)
+
+            updatePlotInfo(station)
+        }
     }
 
+
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+
+    func populateStation() -> CDStation? {
+        let fetchRequest = NSFetchRequest(entityName: "CDStation")
+        fetchRequest.fetchBatchSize = 1
+        fetchRequest.fetchLimit = 1
+        fetchRequest.predicate = NSPredicate(format: "isHidden = NO", argumentArray: nil)
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "order", ascending: true)]
+
+        let stations = Datamanager.sharedManager().managedObjectContext?.executeFetchRequest(fetchRequest, error: nil) as [CDStation]
+
+        return stations.first
+    }
+
+
+    func updatePlotInfo( station: CDStation ) {
+        RHEVindsidenAPIClient.defaultManager().fetchStationsPlotsForStation(station.stationId, completion: { (success:Bool, plots: [AnyObject]!) -> Void in
+            CDPlot.updatePlots(plots, completion: { () -> Void in
+                self.updateUI(station)
+            })
+            }, error: { (cancelled: Bool, error: NSError!) -> Void in
+                DLOG("")
+        })
+    }
+
+
+    func updateUI(station: CDStation) {
+        self.glanceHeadingLabel.setText(station.stationName)
+
+        if let plot = station.lastRegisteredPlot() {
+            let winddir = CGFloat(plot.windDir.floatValue)
+            let windspeed = CGFloat(plot.windAvg.floatValue)
+            let image = DrawArrow.drawArrowAtAngle( winddir, forSpeed:windspeed, highlighted:false, color: UIColor.whiteColor(), hightlightedColor: UIColor.blackColor())
+            self.glanceWindDirectionImage.setImage(image)
+            self.glanceWindCurrentLabel.setText("\(plot.windAvg)")
+        } else {
+            self.glanceWindDirectionImage.setImage(nil)
+            self.glanceWindCurrentLabel.setText("–.–")
+        }
+    }
 }
