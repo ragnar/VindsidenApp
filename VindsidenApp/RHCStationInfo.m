cd78d1be2a3b4b146d92b8690e7c0f771b2f3761
Ragnar Henriksen <ragnarhenriksen@mac.com>
2015-05-15 23:11:53 +0200

    Fixes windDirectionString translation

--- VindsidenApp/RHCStationInfo.m
+++ VindsidenApp/RHCStationInfo.m
@@ -61,54 +61,8 @@
     _windGust.text = (isnan([plot.windMax floatValue]) ? @"—.—" : [NSString stringWithFormat:@"%@ %@", [[self numberFormatter] stringFromNumber:@([plot.windMax speedConvertionTo:unit])], [NSNumber shortUnitNameString:unit]]);
     _windAverage.text = (isnan([plot.windAvg floatValue]) ? @"—.—" : [NSString stringWithFormat:@"%@ %@", [[self numberFormatter] stringFromNumber:@([plot.windAvg speedConvertionTo:unit])], [NSNumber shortUnitNameString:unit]]);
     _windBeaufort.text = (isnan([plot.windAvg floatValue]) ? @"—.—" : [NSString stringWithFormat:@"%0.0f", [plot.windMin speedInBeaufort]]);
-    _windDirection.text = (isnan([plot.windDir floatValue]) ? @"—.—" : [NSString stringWithFormat:@"%0.0f° (%@)", [plot.windDir floatValue], [self windDirectionString:[plot.windDir floatValue]]]);
+    _windDirection.text = (isnan([plot.windDir floatValue]) ? @"—.—" : [NSString stringWithFormat:@"%0.0f° (%@)", [plot.windDir floatValue], [plot windDirectionString]]);
     _tempAir.text = (isnan([plot.tempAir floatValue]) ? @"—.—" : [NSString stringWithFormat:@"%@ °C", [[self numberFormatter] stringFromNumber:plot.tempAir]]);
-}
-
-
-- (NSString *) windDirectionString:(CGFloat)direction
-{
-    if ( direction > 360.0 || direction < 0 ) {
-        direction = 0.0;
-    }
-    NSNumber *dir = @(direction);
-
-
-    if ( [dir isBetween:0.0 and:11.25] || [dir isBetween:348.75 and:360.001]) {
-        return NSLocalizedString(@"DIRECTION_N", @"N");
-    } else if ( [dir isBetween:11.25 and:33.35] ) {
-        return NSLocalizedString(@"DIRECTION_NNE", @"NNE");
-    } else if ( [dir isBetween:33.75 and:56.25] ) {
-        return NSLocalizedString(@"DIRECTION_NE", @"NE");
-    } else if ( [dir isBetween:56.25 and:78.75] ) {
-        return NSLocalizedString(@"DIRECTION_ENE", @"ENE");
-    } else if ( [dir isBetween:78.75 and:101.25] ) {
-        return NSLocalizedString(@"DIRECTION_E", @"E");
-    } else if ( [dir isBetween:101.25 and:123.75] ) {
-        return NSLocalizedString(@"DIRECTION_ESE", @"ESE");
-    } else if ( [dir isBetween:123.75 and:146.25] ) {
-        return NSLocalizedString(@"DIRECTION_SE", @"SE");
-    } else if ( [dir isBetween:146.25 and:168.75] ) {
-        return NSLocalizedString(@"DIRECTION_SSE", @"SSE");
-    } else if ( [dir isBetween:168.75 and:191.25] ) {
-        return NSLocalizedString(@"DIRECTION_S", @"S");
-    } else if ( [dir isBetween:191.25 and:213.75] ) {
-        return NSLocalizedString(@"DIRECTION_SSW", @"SSW");
-    } else if ( [dir isBetween:213.75 and:236.25] ) {
-        return NSLocalizedString(@"DIRECTION_SW", @"SW");
-    } else if ( [dir isBetween:236.25 and:258.75] ) {
-        return NSLocalizedString(@"DIRECTION_WSW", @"WSW");
-    } else if ( [dir isBetween:258.75 and:281.25] ) {
-        return NSLocalizedString(@"DIRECTION_W", @"W");
-    } else if ( [dir isBetween:281.25 and:303.75] ) {
-        return NSLocalizedString(@"DIRECTION_WNW", @"WNW");
-    } else if ( [dir isBetween:303.75 and:326.25] ) {
-        return NSLocalizedString(@"DIRECTION_NW", @"NW");
-    } else if ( [dir isBetween:326.25 and:348.75] ) {
-        return NSLocalizedString(@"DIRECTION_NNW", @"NNW");
-    } else {
-        return NSLocalizedString(@"DIRECTION_UKN", @"UKN");
-    }
 }
 
 
