83a1d39d69cbd948955d9f80038aba1cb4880f7b
Ragnar Henriksen <ragnarhenriksen@mac.com>
2015-08-28 13:51:27 +0200

    Generate graph locally on the watch

--- VindsidenApp WatchKit Extension/RHCGraphInterfaceController.swift
+++ VindsidenApp WatchKit Extension/RHCGraphInterfaceController.swift
@@ -9,6 +9,7 @@
 import WatchKit
 import WatchConnectivity
 import Foundation
+import CoreData
 import VindsidenWatchKit
 
 class RHCGraphInterfaceController: WKInterfaceController {
@@ -23,25 +24,15 @@ class RHCGraphInterfaceController: WKInterfaceController {
         if let station = context as? CDStation {
             stationName.setText(station.stationName)
 
-            let bounds = NSStringFromCGRect(WKInterfaceDevice.currentDevice().screenBounds)
-            let scale = WKInterfaceDevice.currentDevice().screenScale
+            guard let stationId = station.stationId else {
+                DLOG("Missing stationId")
+                return;
+            }
 
-            let userInfo = [
-                "interface": "graph",
-                "action": "update",
-                "station": station.stationId!,
-                "bounds": bounds,
-                "scale": scale
-                ] as [NSObject:AnyObject]
-
-            WCSession.defaultSession().sendMessage(["request":"graph","userInfo":userInfo], replyHandler: { (reply: [String : AnyObject]) -> Void in
-                if let data = reply["graph"] as? NSData {
-                    let image = UIImage(data: data, scale:WKInterfaceDevice.currentDevice().screenScale)
-                    self.graphImage.setImage(image)
-                }
-                }, errorHandler: { (error:NSError) -> Void in
-                    DLOG("\(error)")
-            })
+            let screenScale = WKInterfaceDevice.currentDevice().screenScale
+            let imageData = generateGraphImage(stationId.integerValue, screenSize: WKInterfaceDevice.currentDevice().screenBounds, scale: screenScale)
+            let image = UIImage(data: imageData, scale: screenScale)
+            graphImage.setImage(image)
         }
     }
 
@@ -55,5 +46,34 @@ class RHCGraphInterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+
+
+    func generateGraphImage( stationId: Int, screenSize: CGRect, scale: CGFloat ) -> NSData {
+        let graphImage: GraphImage
+        let imageSize = CGSizeMake( CGRectGetWidth(screenSize), CGRectGetHeight(screenSize) - 40.0)
+
+        if let gregorian = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian) {
+            let inDate = NSDate().dateByAddingTimeInterval(-1*4*3600)
+            let inputComponents = gregorian.components([.Year, .Month, .Day, .Hour], fromDate: inDate)
+            let outDate = gregorian.dateFromComponents(inputComponents)!
+
+            let fetchRequest = NSFetchRequest(entityName: "CDPlot")
+            fetchRequest.predicate = NSPredicate(format: "station.stationId = %ld AND plotTime >= %@", stationId, outDate)
+            fetchRequest.sortDescriptors = [NSSortDescriptor(key: "plotTime", ascending: false)]
+
+            do {
+                let result = try Datamanager.sharedManager().managedObjectContext.executeFetchRequest(fetchRequest) as! [CDPlot]
+                graphImage = GraphImage(size: imageSize, scale: scale, plots: result)
+            } catch {
+                graphImage = GraphImage(size: imageSize, scale: scale)
+            }
+        } else {
+            graphImage = GraphImage(size: imageSize, scale: scale)
+        }
+
+        let image = graphImage.drawImage()
+        return UIImagePNGRepresentation(image)!
     }
 }
